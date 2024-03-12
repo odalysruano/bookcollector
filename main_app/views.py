08118aef4fbebf0548edc7ba028a9632143954d2
odalysruano <odalys.r.ruano@gmail.com>
2024-03-12 13:07:05 -0400

    create Book model

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-books = [
-    {'name': 'Tomorrow, Tomorrow, and Tomorrow', 'author': 'Gabrielle Zevin' , 'description': 'The novel follows the relationship between three friends who begin a successful video game company together.'},
-    {'name': 'Beautiful World, Where Are You', 'author': 'Sally Rooney', 'description': '"Beautiful world, where are you?" is a question two main female characters, best friends from college now on the cusp of 30, grapple with repeatedly in their struggles to figure out how they should live and find meaning in a troubled world that has become increasingly unviable on multiple levels — ecologically, economically, ethically and emotionally.' }
-]
+# Import the Book Model
+from .models import Book
 
 # Create your views here.
 
@@ -15,6 +13,7 @@ def about(request):
     return render(request, 'about.html')
 
 def books_index(request):
+    books = Book.objects.all() # Retrieve all books
     return render(request, 'books/index.html', {
         'books': books
 })
