3330a2a23e816d825e90e7fc46848aad18a4e237
odalysruano <odalys.r.ruano@gmail.com>
2024-03-17 17:16:23 -0400

    add Genre model and create many-to-many relationship

--- main_app/models.py
+++ main_app/models.py
@@ -3,11 +3,20 @@ from django.urls import reverse
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Genre(models.Model):
+    type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.type
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     year_published = models.IntegerField()
+
+    genre = models.ManyToManyField(Genre)
 
     def __str__(self):
         return self.title
